fdc2699f41b87b3711f07f73200b4fd83bf806a2
Mariam <yayaetud@gmail.com>
2024-03-31 20:44:30 +0000

    First Commit

---
+++ src/test/java/org/sid/comptebancaire/CompteBancaireApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.comptebancaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteBancaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
